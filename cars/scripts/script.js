62aee87cd9f122c413756cbbb0e662c5c26683dd
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-09-23 21:24:47 +0300

    4's homework

--- cars/scripts/script.js
+++ cars/scripts/script.js
@@ -12,11 +12,23 @@ for (let i = 0; i < buttons.length; i++) {
 document.getElementById("price-action").onclick = function () {
   if (document.getElementById("name").value === '') {
     alert('Заполните поле имя');
-  } else  if (document.getElementById("phone").value === '') {
+  } else if (document.getElementById("phone").value === '') {
     alert('Заполните поле телефон');
-  } else  if (document.getElementById("car").value === '') {
+  } else if (document.getElementById("car").value === '') {
     alert('Заполните поле автомобиль');
   } else {
     alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let layer = document.querySelector('.price-image');
+  document.addEventListener('mousemove', (event) => {
+    layer.style.transform = 'translate3d(' + ((event.clientX * 1) / 8) + 'px,' + ((event.clientY * 0.3) / 8) + 'px,0px)';
+  });
+
+  const elem = document.querySelector(".main");
+  document.addEventListener('scroll', () => {
+    elem.style.backgroundPositionX = '0' + (0.6 * window.pageYOffset) + 'px';
+  })
+});
